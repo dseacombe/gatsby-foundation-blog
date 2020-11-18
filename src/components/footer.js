efe463db81c6869550f94069f2f919495f5658f7
dseacombe <dseacombe@users.noreply.github.com>
2020-11-18 15:40:48 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-    <p>&copy;&nbsp; new Date().toISOString().substr(0,4);&nbsp;David Seacombe <span> <script type="text/javascript">document.write(new Date().getFullYear())</script> </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:ds@2ir.io">ds@2ir.io</a></p>
+    <p>&copy;&nbsp;David Seacombe 2020&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:ds@2ir.io">ds@2ir.io</a></p>
     
     </div>
   </footer>
